54331d21ddffc0f6ea99d26e5a088a91a017a71c
MEklit_Asrat <meklitzasrat@gmai.com>
2024-01-05 05:40:41 +0300

    About-us sample

---
+++ src/app.jsx
@@ -0,0 +1,137 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   
+   <div className='relative'>
+
+    <nav  className='fixed top-0 left-0 right-0 text-xl font-bold text-stone-50 border-2  bg-blue-500 grid grid-cols-3 gap-5 rounded justify-around '>
+
+    <div className='flex p-3 m-5'>
+      <img src='./src/Efoyta logo.png' className='p-1 object-scale-down w-20  h-30 m-2 rounded-2xl  '></img>
+     <div className='flex justify-center align-center p-5 '> <p className='text-centered text-xl align-center'> Efoyta Doctor's Appointment Booking </p> </div> 
+    </div>
+
+    <ul className='text-1xl font-bold text-black-500  flex m-5 p-5'> 
+      
+      <li className='p-5 '> <a href="#">  Home </a> </li>
+      <li className='p-5'> <a href="#">   About_Us</a> </li>
+      <li className='p-5'> <a href="#"> Find Doctor </a> </li>
+      <li className='p-5 '> <a href="#">  Contact Us</a> </li>
+      
+     </ul>
+
+     <div className='text-1xl font-bold text-black-500 flex m-5 p-5'> 
+      <div className='p-5'> 
+        <p> <a href="#"> Login</a> </p>
+
+      </div>
+
+      <div className='p-5'>
+        <p> <a href=""> Signup</a> </p>
+
+      </div>
+     </div>
+
+    </nav>
+     
+    
+
+     <div className='mb-5  m-5 p-3 rounded'>
+      <p className='text-3xl border-2  m-5 p-5 rounded my-7 '> About Us</p>
+     </div>
+
+     <div className='mb-5 bg-blue-200 m-5 p-2 rounded'>
+      <p className='text-3xl   m-5 p-5 rounded my-7 '> About Us</p>
+     </div>
+
+     <p className='text-center m-5 p-5 text-2xl'> Efoyta Doctor's Appointment Booking System</p>
+     <div >
+
+     <div className='flex justify-center align-center p-3 m-5'> <img src='./src/Efoyta logo.png' className='border-4 rounded m-5 shadow-2xl'></img> </div>
+     
+    
+     <div className='p-5 m-5 '> <p className='border-2 p-4 m-5 rounded shadow-2xl'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam porro velit animi veniam cumque ad molestiae cupiditate, aliquid pariatur dolorum mollitia, nulla optio maiores aperiam assumenda iure blanditiis reprehenderit nam eaque. Dolorum incidunt non laborum sed, perferendis sunt sint, accusantium, nobis mollitia laboriosam est exercitationem praesentium nulla magni tempora accusamus.</p> </div>
+
+     </div>
+
+     <div className='grid grid-cols-2 m-3 my-7'>
+      <img src='./src/Efoyta logo.png' className='p-5 m-5'></img>
+      <div className='flex justify-center align-center mt-7 p-5 mx-5 border-2 shadow-2xl'>  <p className='flex justify-center align-center p-5'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta facere consectetur, ipsum blanditiis dolore delectus modi incidunt, possimus, id praesentium error dolorem alias quod ratione reprehenderit dignissimos distinctio ea cumque numquam rem. Dolores nobis dolore saepe eveniet dolor nam, enim modi ipsam velit repudiandae quisquam vel quod at dicta harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui sit vitae illum odit unde autem. Error voluptatibus sed pariatur.</p></div>
+     </div>
+
+
+     <div className='grid grid-cols-2 m-3 my-7'>
+      <div className='flex justify-center align-center mt-7 p-5 mx-5 border-2 shadow-2xl'>  <p className='flex justify-center align-center p-5'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta facere consectetur, ipsum blanditiis dolore delectus modi incidunt, possimus, id praesentium error dolorem alias quod ratione reprehenderit dignissimos distinctio ea cumque numquam rem. Dolores nobis dolore saepe eveniet dolor nam, enim modi ipsam velit repudiandae quisquam vel quod at dicta harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui sit vitae illum odit unde autem. Error voluptatibus sed pariatur.</p></div>
+      <img src='./src/Efoyta logo.png' className='p-5 m-5'></img>
+     </div>
+
+     <div className='grid grid-cols-2 m-3 my-7'>
+      <img src='./src/Efoyta logo.png' className='p-5 m-5'></img>
+      <div className='flex justify-center align-center mt-7 p-5 mx-5 border-2 shadow-2xl'>  <p className='flex justify-center align-center p-5'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta facere consectetur, ipsum blanditiis dolore delectus modi incidunt, possimus, id praesentium error dolorem alias quod ratione reprehenderit dignissimos distinctio ea cumque numquam rem. Dolores nobis dolore saepe eveniet dolor nam, enim modi ipsam velit repudiandae quisquam vel quod at dicta harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui sit vitae illum odit unde autem. Error voluptatibus sed pariatur.</p></div>
+     </div>
+
+
+     <footer className=' bg-gray-200'>
+      
+      <div className='my-5 border-2 p-5 bg-gray-200 grid grid-cols-3'>
+
+      <div>
+          <img src='./src/Efoyta logo.png' className= 'p-5 m-5  object-scale-down w-20 h-50 m-2 rounded'></img>
+          <p className='m-5 p-5 text-bold text-xl underline'> Ease your appointment schedule with Efoyta </p>
+          <div>
+            <ul className='p-5 m-5'>
+              <li className='p-3'> Email</li>
+              <li className='p-3'> Twitter </li>
+              <li className='p-3'> Telegram </li>
+              <li className='p-3'> Instagram</li>
+            </ul>
+          </div>
+        
+        </div>
+
+
+        
+
+        <div className='p-5 m-5 '>
+        <p className='text-bold text-2xl p-5 m-5 underline'> Contact Us </p>
+        <ul className=' p-5 m-5'>
+              <li className='p-3'> Email</li>
+              <li className='p-3'> Twitter </li>
+              <li className='p-3'> Telegram </li>
+              <li className='p-3'> Instagram</li>
+            </ul>
+
+        </div>
+
+        <div className='p-5 m-5 '>
+          <p className='p-5 m-5 text-bold text-2xl underline'> Our Services </p>
+        <ul className='p-5 m-5'>
+              <li className='p-3'> * Lab test Request and Review </li>
+              <li className='p-3'> * Booking Appointment </li>
+              <li className='p-3'> * Transiting Payment </li>
+              <li className='p-3'> * Instagram</li>
+            </ul>
+        </div>
+
+      </div>
+       
+       <div className='bg-slate-900 text-white p-3'>
+        <p className='text-center'> ©copy right is reserved | Jan, 2024 | protected by law</p>
+       </div>
+       
+      
+     </footer>
+
+   </div>
+
+ 
+    
+  )
+   
+}
+
+export default App
